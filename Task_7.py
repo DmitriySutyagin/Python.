fcca72ed6e5fdc3803d658a6f6e1a1372c4da322
Dmitriy <Sutyagin15086733@yandex.ru>
2022-10-14 21:31:39 +0300

    Доработали 7 задачу

--- Task_7.py
+++ Task_7.py
@@ -1,7 +1,11 @@
 # Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
 # Пример:
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
-number_n = int(input('Eneter number n: '))
+
+number_n = input('Eneter number n: ')
+while number_n.isalpha():
+    number_n = input('Eneter number n: ')
+number_n = int(number_n)    
 mul = 1
 i = 1
 while i != number_n + 1:
