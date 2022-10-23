5e9b4ba352382e447ba6ee8c656a009e8a64bbc3
Dmitriy <Sutyagin15086733@yandex.ru>
2022-10-23 16:47:47 +0300

    Решение 18 задачи

---
+++ Task_18.py
@@ -0,0 +1,14 @@
+# Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+number_n = input('How many numbers do you want to enter?')
+while number_n.isalpha():
+    number_n = input('How many numbers do you want to enter?')
+number_n = int(number_n)
+
+lines = [int(input()) for _ in range(int(number_n))] 
+print(lines)
+list = []
+for i in lines:
+    if i not in list:
+        list.append(i)
+         
+print(list)
